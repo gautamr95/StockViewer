a9a3113df2dfac25ac9f081ed7f8dc015ff4f10e
Gautam Ramaswamy <grr37@cornell.edu>
2015-06-19 15:26:46 -0400

    Add navigation drawer

--- app/src/main/java/com/example/gautam/stockviewer/MainActivity.java
+++ app/src/main/java/com/example/gautam/stockviewer/MainActivity.java
@@ -1,93 +1,46 @@
 package com.example.gautam.stockviewer;
 
-import android.content.Context;
-import android.graphics.Paint;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.github.mikephil.charting.charts.Chart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
-    final String LOG_TAG = this.getClass().getSimpleName();
 
-    private Button mBtn;
-    private EditText mInputEText;
-    private EditText mNumberEText;
-    private TextView mOutputTView;
-    private TextView mDateTView;
-    private TextView mNameTView;
-    private LineChart mChart;
+    private String[] mNavTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtn = (Button) findViewById(R.id.search_btn);
-        mInputEText = (EditText) findViewById(R.id.input);
-        mNameTView = (TextView) findViewById(R.id.name);
-        mOutputTView = (TextView) findViewById(R.id.price_today);
-        mDateTView = (TextView) findViewById(R.id.date_today);
-        mChart = (LineChart) findViewById(R.id.chart);
-        mNumberEText = (EditText) findViewById(R.id.number);
 
-        Paint p = mChart.getPaint(Chart.PAINT_INFO);
-        p.setColor(getResources().getColor(R.color.primary_dark_material_light));
-        InputMethodManager imm = (InputMethodManager) getSystemService(
-                Context.INPUT_METHOD_SERVICE);
+        mNavTitles = getResources().getStringArray(R.array.nav_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, mNavTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        mBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String input = mInputEText.getText().toString();
-                String number = mNumberEText.getText().toString();
-                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (input.length() == 0) {
-                    Toast.makeText(getApplicationContext(), "Please input a stock first", Toast.LENGTH_SHORT).show();
-                } else if (number.length() == 0) {
-                    Toast.makeText(getApplicationContext(), "Please input the number of days", Toast.LENGTH_SHORT).show();
-                } else if (networkInfo != null && networkInfo.isConnected()) {
-                    new getStockInfo().execute(input, number);
-                } else {
-                    Toast.makeText(getApplicationContext(), "You are not connected to the Internet", Toast.LENGTH_SHORT).show();
-                }
-                InputMethodManager imm = (InputMethodManager) getSystemService(
-                        Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(mInputEText.getWindowToken(), 0);
-                imm.hideSoftInputFromWindow(mNumberEText.getWindowToken(), 0);
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
     @Override
@@ -108,132 +61,39 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private class getStockInfo extends AsyncTask<String, Void, String> {
-        final String baseUrl = "https://www.quandl.com/api/v1/datasets/WIKI/";
-        final String APP_ID = "rNviB11yaDTGF8ycmsfY";
-        final String ID_PARAM = "auth_token";
-        final String rows = "rows";
-        final String cols = "column";
-        String numRow = "1";
-        final String numCol = "4";
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
 
-//        final String LOG_TAG = this.getClass().getSimpleName();
-
-        String name = null;
-        String date = null;
-        Double price = null;
-
-        LineData lineData = null;
-
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
-        protected String doInBackground(String... params) {
-            HttpURLConnection urlConnection;
-            String input = params[0];
-            numRow = params[1];
-            InputStream inputStream;
-            String stockString = null;
-            try {
-                Uri builtUri = Uri.parse(baseUrl).buildUpon().appendPath(input + ".json").appendQueryParameter(ID_PARAM, APP_ID)
-                        .appendQueryParameter(rows, numRow).appendQueryParameter(cols, numCol).build();
-                URL url = new URL(builtUri.toString());
-                Log.v(LOG_TAG, url.toString());
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                if (inputStream == null) {
-                    return null;
-                }
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuffer buffer = new StringBuffer();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-                if (buffer.length() == 0) {
-                    return null;
-                }
-                stockString = buffer.toString();
-                getTodaysPrice(stockString);
-                getDataRange(stockString);
-//                Log.v(LOG_TAG, stockString);
-            } catch (IOException e) {
-//                Log.e(LOG_TAG, "Bad site");
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Incorrect Ticker", Toast.LENGTH_LONG).show();
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return stockString;
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
         }
+    }
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            if (name != null)
-                mNameTView.setText(name);
-            if (date != null)
-                mDateTView.setText(date);
-            if (price != null)
-                mOutputTView.setText(Double.toString(price));
-            if (lineData != null) {
-                mChart.setData(lineData);
-                mChart.setDescription("");
-                mChart.invalidate(); // refresh
-            }
-        }
-
-        private void getTodaysPrice(String stockJsonStr) {
-            final String COL_NAMES = "column_names";
-            final String DATA = "data";
-            final String NAME = "name";
-            try {
-                JSONObject stockJson = new JSONObject(stockJsonStr);
-                JSONArray names = stockJson.getJSONArray(COL_NAMES);
-                JSONArray data = stockJson.getJSONArray(DATA).getJSONArray(0);
-                name = stockJson.getString(NAME);
-                name = name.substring(0, name.indexOf(")") + 1);
-                date = data.getString(0);
-                price = data.getDouble(1);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "JSON Error!");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void getDataRange(String stockJsonStr) {
-            final String COL_NAMES = "column_names";
-            final String DATA = "data";
-            ArrayList<Entry> vals = new ArrayList<Entry>();
-            try {
-                JSONObject stockJson = new JSONObject(stockJsonStr);
-                JSONArray names = stockJson.getJSONArray(COL_NAMES);
-                JSONArray data = stockJson.getJSONArray(DATA);
-                ArrayList<String> xVals = new ArrayList<String>();
-                int len = data.length();
-                for (int i = len - 1; i >= 0; i--) {
-                    JSONArray obj = data.getJSONArray(i);
-                    vals.add(new Entry((float) obj.getDouble(1), len - 1 - i));
-//                    xVals.add(String.valueOf(len - i));
-                    xVals.add(obj.getString(0));
-                }
-                LineDataSet dataSet = new LineDataSet(vals, name);
-                dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-                lineData = new LineData(xVals, dataSet);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "JSON Error!");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    private void selectItem(int position) {
+        Fragment fragment = new StockViewFragment();
+//        Bundle args = new Bundle();
+//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        fragment.setArguments(args);
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        mDrawerList.setItemChecked(position, true);
+//        setTitle(mNavTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
 }
---
+++ app/src/main/java/com/example/gautam/stockviewer/StockViewFragment.java
@@ -0,0 +1,308 @@
+package com.example.gautam.stockviewer;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Paint;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link StockViewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link StockViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StockViewFragment extends Fragment {
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+
+    final String LOG_TAG = this.getClass().getSimpleName();
+
+    private Button mBtn;
+    private EditText mInputEText;
+    private EditText mNumberEText;
+    private TextView mOutputTView;
+    private TextView mDateTView;
+    private TextView mNameTView;
+    private LineChart mChart;
+
+//    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StockViewFragment.
+     */
+//    // TODO: Rename and change types and number of parameters
+//    public static StockViewFragment newInstance(String param1, String param2) {
+//        StockViewFragment fragment = new StockViewFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+    public StockViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_stock_view, container,
+                false);
+        mBtn = (Button) rootView.findViewById(R.id.search_btn);
+        mInputEText = (EditText) rootView.findViewById(R.id.input);
+        mNameTView = (TextView) rootView.findViewById(R.id.name);
+        mOutputTView = (TextView) rootView.findViewById(R.id.price_today);
+        mDateTView = (TextView) rootView.findViewById(R.id.date_today);
+        mChart = (LineChart) rootView.findViewById(R.id.chart);
+        mNumberEText = (EditText) rootView.findViewById(R.id.number);
+
+        Paint p = mChart.getPaint(Chart.PAINT_INFO);
+        p.setColor(getResources().getColor(R.color.primary_dark_material_light));
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = mInputEText.getText().toString();
+                String number = mNumberEText.getText().toString();
+                ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (input.length() == 0) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please input a stock first", Toast.LENGTH_SHORT).show();
+                } else if (number.length() == 0) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Please input the number of days", Toast.LENGTH_SHORT).show();
+                } else if (networkInfo != null && networkInfo.isConnected()) {
+                    new getStockInfo().execute(input, number);
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "You are not connected to the Internet", Toast.LENGTH_SHORT).show();
+                }
+                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
+                        Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mInputEText.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(mNumberEText.getWindowToken(), 0);
+            }
+        });
+        return rootView;
+    }
+
+
+    private class getStockInfo extends AsyncTask<String, Void, String> {
+        final String baseUrl = "https://www.quandl.com/api/v1/datasets/WIKI/";
+        final String APP_ID = "rNviB11yaDTGF8ycmsfY";
+        final String ID_PARAM = "auth_token";
+        final String rows = "rows";
+        final String cols = "column";
+        String numRow = "1";
+        final String numCol = "4";
+
+//        final String LOG_TAG = this.getClass().getSimpleName();
+
+        String name = null;
+        String date = null;
+        Double price = null;
+
+        LineData lineData = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection;
+            String input = params[0];
+            numRow = params[1];
+            InputStream inputStream;
+            String stockString = null;
+            try {
+                Uri builtUri = Uri.parse(baseUrl).buildUpon().appendPath(input + ".json").appendQueryParameter(ID_PARAM, APP_ID)
+                        .appendQueryParameter(rows, numRow).appendQueryParameter(cols, numCol).build();
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, url.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer buffer = new StringBuffer();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                stockString = buffer.toString();
+                getTodaysPrice(stockString);
+                getDataRange(stockString);
+//                Log.v(LOG_TAG, stockString);
+            } catch (IOException e) {
+//                Log.e(LOG_TAG, "Bad site");
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(getActivity().getApplicationContext(), "Incorrect Ticker", Toast.LENGTH_LONG).show();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return stockString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (name != null)
+                mNameTView.setText(name);
+            if (date != null)
+                mDateTView.setText(date);
+            if (price != null)
+                mOutputTView.setText(Double.toString(price));
+            if (lineData != null) {
+                mChart.setData(lineData);
+                mChart.setDescription("");
+                mChart.invalidate(); // refresh
+            }
+        }
+
+        private void getTodaysPrice(String stockJsonStr) {
+            final String COL_NAMES = "column_names";
+            final String DATA = "data";
+            final String NAME = "name";
+            try {
+                JSONObject stockJson = new JSONObject(stockJsonStr);
+                JSONArray names = stockJson.getJSONArray(COL_NAMES);
+                JSONArray data = stockJson.getJSONArray(DATA).getJSONArray(0);
+                name = stockJson.getString(NAME);
+                name = name.substring(0, name.indexOf(")") + 1);
+                date = data.getString(0);
+                price = data.getDouble(1);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "JSON Error!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void getDataRange(String stockJsonStr) {
+            final String COL_NAMES = "column_names";
+            final String DATA = "data";
+            ArrayList<Entry> vals = new ArrayList<Entry>();
+            try {
+                JSONObject stockJson = new JSONObject(stockJsonStr);
+                JSONArray names = stockJson.getJSONArray(COL_NAMES);
+                JSONArray data = stockJson.getJSONArray(DATA);
+                ArrayList<String> xVals = new ArrayList<String>();
+                int len = data.length();
+                for (int i = len - 1; i >= 0; i--) {
+                    JSONArray obj = data.getJSONArray(i);
+                    vals.add(new Entry((float) obj.getDouble(1), len - 1 - i));
+//                    xVals.add(String.valueOf(len - i));
+                    xVals.add(obj.getString(0));
+                }
+                LineDataSet dataSet = new LineDataSet(vals, name);
+                dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                lineData = new LineData(xVals, dataSet);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "JSON Error!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+}
